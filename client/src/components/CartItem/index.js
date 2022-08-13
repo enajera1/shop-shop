a449148475ad33158e4f80276c6fa88c2bb45dd3
enajera1 <eriknajera013@gmail.com>
2022-08-13 14:50:45 -0500

    indexedDB added. cart persistence complete

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,8 @@
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import React from 'react';
+//IndexedDB
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
@@ -11,25 +13,29 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id
     });
-  }; 
+    idbPromise('cart', 'delete', { ...item });
+  };
 
   const onChange = (e) => {
     const value = e.target.value;
   
-    if (value === '0') {
-      dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-      });
-    } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: item._id,
-        purchaseQuantity: parseInt(value)
-      });
-    }
-  };
-
+      if (value === '0') {
+        dispatch({
+          type: REMOVE_FROM_CART,
+          _id: item._id
+        });
+      
+        idbPromise('cart', 'delete', { ...item });
+      } else {
+        dispatch({
+          type: UPDATE_CART_QUANTITY,
+          _id: item._id,
+          purchaseQuantity: parseInt(value)
+        });
+      
+        idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+      }
+  } 
   return (
     <div className="flex-row">
       <div>
